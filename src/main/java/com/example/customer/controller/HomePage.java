53dfaaf82402c3858672f5b46d473b400791a77f
Anahitashd <anahita_s76@yahoo.com>
2025-09-15 15:22:12 +0330

    thymleaf

---
+++ src/main/java/com/example/customer/controller/HomePage.java
@@ -0,0 +1,16 @@
+package com.example.customer.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePage {
+
+    @GetMapping("/login")   // mapping for home page
+    public String home(Model model) {
+        model.addAttribute("message", "Welcome to the Bank Project!");
+        return "index"; // matches templates/index.html
+    }
+}
