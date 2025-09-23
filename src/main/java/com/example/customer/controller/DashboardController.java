1bd522cdae1369d1032eb324f444d9a74ea29c52
Anahitashd <anahita_s76@yahoo.com>
2025-09-23 11:46:46 +0330

    add ui and dashboard

--- src/main/java/com/example/customer/controller/DashboardController.java
+++ src/main/java/com/example/customer/controller/DashboardController.java
@@ -1,7 +1,6 @@
 package com.example.customer.controller;
 
 import com.example.customer.model.Customer;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +23,6 @@ public class DashboardController {
         String apiUrl = "http://localhost:8080/api/accounts";
         ResponseEntity<Customer[]> response = restTemplate.getForEntity(apiUrl, Customer[].class);
         List<Customer> accounts = Arrays.asList(response.getBody());
-
         model.addAttribute("accounts", accounts);
         return "dashboard"; // dashboard.html must exist
     }
--- src/main/java/com/example/customer/model/Customer.java
+++ src/main/java/com/example/customer/model/Customer.java
@@ -1,8 +1,7 @@
 package com.example.customer.model;
 
+
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.lang.NonNull;
 import java.util.ArrayList;
 import java.util.List;
