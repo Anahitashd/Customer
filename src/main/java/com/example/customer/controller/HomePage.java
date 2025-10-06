5c00386b9ed3f083caed17a36c75880db4e59f58
Anahitashd <anahita_s76@yahoo.com>
2025-10-06 15:59:30 +0330

    add customer

--- src/main/java/com/example/customer/controller/HomePage.java
+++ src/main/java/com/example/customer/controller/HomePage.java
@@ -2,13 +2,9 @@ package com.example.customer.controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Objects;
 
 @Controller
 public class HomePage {
